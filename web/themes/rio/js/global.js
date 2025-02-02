5a394448053a8e75ec98aaf044d67fa27c007385
SahilGidwani <sahilgidwani98@gmail.com>
2025-02-02 14:33:43 +0530

    Resposive implementation

--- web/themes/rio/js/global.js
+++ web/themes/rio/js/global.js
@@ -2,16 +2,32 @@
  * @file
  * Rio behaviors.
  */
-(function (Drupal, once) {
-
-  'use strict';
+(function (Drupal, $, once) {
+  "use strict";
 
   Drupal.behaviors.rio = {
-    attach (context, settings) {
-
-      console.log('It works!');
-
-    }
+    attach(context, settings) {
+      function MobileMenuClick(element) {
+        if (element.hasClass("active")) {
+          element.removeClass("active");
+          $("body").removeClass("mobile-menu-open");
+          $(".header-mobile-menu").slideUp();
+        } else {
+          element.addClass("active");
+          $("body").addClass("mobile-menu-open");
+          $(".header-mobile-menu").slideDown();
+        }
+      }
+      $(once("menuToggle", "#menuToggle", context)).click(function () {
+        MobileMenuClick($(this));
+      });
+      $(once("menuTogglekeypress", $("#menuToggle"), context)).keypress(
+        function (e) {
+          if (e.which == 13) {
+            MobileMenuClick($(this));
+          }
+        }
+      );
+    },
   };
-
-} (Drupal, once));
+})(Drupal, jQuery, once);
